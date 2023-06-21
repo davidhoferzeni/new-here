ce4b429f53855912d345868aba4b85747ebb9ee3
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-06-21 15:16:57 +0200

    Added lit-html to render string content

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,4 +1,8 @@
-// Create a class for the element
+import htmlTemplate from "./popup.html?raw";
+import cssTemplate from "./popup.css?raw";
+import { html, render } from "lit-html";
+import { unsafeHTML } from "lit-html/directives/unsafe-html.js";
+
 class PopUpInfo extends HTMLElement {
   static get observedAttributes() {
     return ["data-text"];
@@ -10,70 +14,12 @@ class PopUpInfo extends HTMLElement {
 
     // Create a shadow root
     const shadow = this.attachShadow({ mode: "open" });
-
-    // Create spans
-    const wrapper = document.createElement("span");
-    wrapper.setAttribute("class", "wrapper");
-
-    const icon = document.createElement("span");
-    icon.setAttribute("class", "icon");
-
-    this.#info = document.createElement("span");
-    this.#info.setAttribute("class", "info");
-
-    // Take attribute content and put it inside the info span
-    const text = this.getAttribute("data-text");
-    this.#info.textContent = text;
-
-    // Create some CSS to apply to the shadow dom
+    const sayHi = html`${unsafeHTML(htmlTemplate)}`;
+    render(sayHi, shadow);
     const style = document.createElement("style");
-    console.log(style.isConnected);
-
-    style.textContent = `
-      .wrapper {
-        position: relative;
-        display: block;
-        margin: 2rem;
-      }
-
-      .info {
-        font-size: 0.8rem;
-        width: 200px;
-        display: inline-block;
-        border: 1px solid black;
-        padding: 10px;
-        background: white;
-        border-radius: 10px;
-        opacity: 0;
-        transition: 0.3s all;
-        position: absolute;
-        bottom: -20px;
-        left: 10px;
-        z-index: 3;
-      }
-
-      .icon {
-        background: blue;
-        display: block;
-        width: 2rem;
-        height: 2rem;
-      }
-
-      .icon:hover + .info, .icon:focus + .info {
-        opacity: 1;
-      }
-    `;
-
-    // Attach the created elements to the shadow dom
+    style.textContent = cssTemplate;
     shadow.appendChild(style);
-    console.log(style.isConnected);
-    shadow.appendChild(wrapper);
-    wrapper.appendChild(icon);
-    wrapper.appendChild(this.#info);
   }
-
-  /** @type {HTMLSpanElement} */
-  #info;
 
   /**
    * @param {string} name
@@ -82,9 +28,6 @@ class PopUpInfo extends HTMLElement {
    */
   attributeChangedCallback(name, oldValue, newValue) {
     console.log(`Custom element attribute ${name} changed.`);
-    if (name === "data-text") {
-      this.#info.textContent = newValue;
-    }
   }
 }
 
